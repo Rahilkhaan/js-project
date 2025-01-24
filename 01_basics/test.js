e2730cf31a2430ee005e5341e55635735a8e0630
Rahilkhaan <97218543+Rahilkhaan@users.noreply.github.com>
2025-01-24 17:48:45 +0000

    name changes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
+console.log("usman");
